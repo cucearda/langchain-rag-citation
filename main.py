f93dfaaeb702b1c57a9f41fcd60807e3162f9116
cucearda <cuce.arda1999@gmail.com>
2026-03-16 00:01:56 +0100

    Cors allow origins update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://rag-citation-chakra-production.up.railway.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
